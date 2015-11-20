37bb442e37ef4b55b0823d61adaca74ff2dfb13a
ltindi1 <leroy.tindi@gmail.com>
2015-11-20 11:21:16 -0500

    Create index_search.php

---
+++ index_search.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"       
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">       
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">       
+  <head>       
+    <title>Poison Ivy</title>       
+    <meta http-equiv="content-type"       
+        content="text/html; charset=utf-8"/>   
+    <link href="css/styles.css" rel="stylesheet">    
+  </head>       
+  <body>   
+  
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+      
+  	<center>
+  	
+  		<h2> Poison Ivy </h2> //Header
+  		
+  		<!--Search form that uses get method and search.php-->
+  		<form action='./search.php' method='get'>
+  			<input type='text' name ='k' size = '50' />
+  			<input type='submit' value='Search'>	 
+  		</form>
+  	
+  	</center>         
+  </body>       
+</html>
